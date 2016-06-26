93f8e81ea16d8c10f8b90f1ed9f72211eebefd5a
Bill Wu <bill.wu@wulei.ca>
2016-06-26 08:21:16 -0500

    tweek the display of size which have plan size and aua together.

--- sites/all/themes/corposs/templates/views/ip-geoloc-map.tpl.php
+++ sites/all/themes/corposs/templates/views/ip-geoloc-map.tpl.php
@@ -41,7 +41,7 @@ $num = count($view->style_plugin->rendered_fields);
 <?php if ($view->current_display === 'map_search' && $num > 0) : ?>
 <div class="ip-geoloc-table">
 <table class="ip-geoloc-map-table views-table cols-5">
-  <thead><tr><th class="views-field">Name</th><th class="views-field">Address</th class="views-field"><th>Type</th><th>Plan Size</th></tr></thead><tbody>
+  <thead><tr><th class="views-field">Name</th><th class="views-field">Address</th class="views-field"><th>Type</th><th>Size</th></tr></thead><tbody>
   <?php foreach ($view->style_plugin->rendered_fields as $key => $wrow) : 
     $row_class = 'class = "';
     $row_class .= ($key % 2 == 0) ? 'odd' : 'even';
@@ -51,7 +51,7 @@ $num = count($view->style_plugin->rendered_fields);
     <td><?php print $wrow['field_consultant_firm_er'] . '<br>'; ?><?php print (strpos($wrow['field_content_type_human_tr'],'Consultant Office') === FALSE) ? $wrow['title_field'] : strip_tags($wrow['title_field']) . ' Office'; ?></td>
     <td><?php print $wrow['field_address']; ?></td>
     <td><?php print $wrow['field_content_type_human_tr']; ?></td>
-    <td><?php print $wrow['field_plan_size']; ?></td>
+    <td><?php print $wrow['nothing_1']; ?></td>
   </tr>
   <?php endforeach; ?>
 </tbody></table>
