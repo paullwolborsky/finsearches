c1b14c1fa2fc1d04973d47db01c6b0a1de42f440
Bill Wu <bill.wu@wulei.ca>
2016-06-30 19:53:27 -0500

    fix the vertical alignment of contact name

--- sites/all/themes/corposs/templates/views/views-view-field--reference-views--plan-lead-contact--field-lead-plan-contact.tpl.php
+++ sites/all/themes/corposs/templates/views/views-view-field--reference-views--plan-lead-contact--field-lead-plan-contact.tpl.php
@@ -1,10 +1,9 @@
 <?php
 $lead = $row->field_field_lead_plan_contact[0]['raw']['value'];
 if ($lead) {
-  $leadstr = t('Lead');
+  $leadstr = "<div class='lead-contact'>" . t('Lead') . '</div>';
 }
 else {
-  $leadstr = '';
+  $leadstr = ' ';
 }
-?>
-<div class='lead-contact'><?php print $leadstr; ?></div>
+print $leadstr;
